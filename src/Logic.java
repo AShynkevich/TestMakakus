38349be0c3b8a47c7073f31be92f315bd6621c8c
Aliaksandr Shynkevich <al3xshaman@gmail.com>
2016-03-01 21:44:15 +0300

    Code review with fixing.

--- src/Logic.java
+++ src/Logic.java
@@ -5,36 +5,41 @@ import java.io.InputStreamReader;
 /**
  * Created by Deniel on 26.02.2016.
  */
-public class Logic{
+public class Logic {
 
-    public Logic() throws IOException, InterruptedException {
-        logic();
-    }
-    public void logic () throws IOException, InterruptedException {
+    public void logic() throws IOException, InterruptedException {
         int button = 0;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Menu menu = new Menu(); // создание объекта, ровно один раз!!!
         while (button != 4) {
-            new Menu ();
+            menu.menu(); // вызов метода menu() при каждом проходе цикла
             String s = reader.readLine();
-
             try {
                 button = Integer.parseInt(s);
             } catch (NumberFormatException e) {
-                System.out.println("Press only NUMBERS, YOU FUCKING IDIOT!!!");
+                System.out.println("Press only NUMBERS"); // без ругательств
                 continue;
             }
 
-            switch (button)
-            {
-                case 1: System.out.println("Make order"); break;
-                case 2: System.out.println("Load order"); break;
-                case 3: System.out.println("Get order"); break;
-                case 4: System.exit(0); break;
-                default: System.out.println("I think, you need some 'format C:/' or tea with polonium. Just choose fucking number...");
+            switch (button) {
+                case 1:
+                    System.out.println("Make order");
+                    break;
+                case 2:
+                    System.out.println("Load order");
+                    break;
+                case 3:
+                    System.out.println("Get order");
+                    break;
+                case 4:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("I think, you need some 'format C:/' or tea with polonium. Just choose fucking number...");
             }
-            for ( int i = 0; i < 25; ++i ) {
+            for (int i = 0; i < 25; ++i) {
                 System.out.println();
-                Thread.sleep(100);
+                Thread.sleep(100); // это для чего?
             }
         }
     }
--- src/Menu.java
+++ src/Menu.java
@@ -1,12 +1,9 @@
 /**
  * Created by Deniel on 26.02.2016.
  */
-
 public class Menu {
-    public Menu (){
-        menu();
-    }
-    public void menu (){
+
+    public void menu() {
         System.out.println("1) Make Order");
         System.out.println("2) Load Order");
         System.out.println("3) Get Order");
