2016153adfba1f06f2bf59b35e00dd17f01ebb30
Deniel089 <deniel089@gmail.com>
2016-03-02 10:57:24 +0300

    Rebuild
    Cleaning. "Press any key" realized.

--- src/Logic.java
+++ src/Logic.java
@@ -7,17 +7,19 @@ import java.io.InputStreamReader;
  */
 public class Logic {
 
-    public void logic() throws IOException, InterruptedException {
+    public void logic() throws IOException {
         int button = 0;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Menu menu = new Menu(); // создание объекта, ровно один раз!!!
+        Menu menu = new Menu();
         while (button != 4) {
-            menu.menu(); // вызов метода menu() при каждом проходе цикла
+            menu.menu();
             String s = reader.readLine();
             try {
                 button = Integer.parseInt(s);
             } catch (NumberFormatException e) {
-                System.out.println("Press only NUMBERS"); // без ругательств
+                System.out.println("Press only NUMBERS");
+                System.out.println("Press any key to continue...");
+                String r = reader.readLine();
                 continue;
             }
 
@@ -35,11 +37,12 @@ public class Logic {
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("I think, you need some 'format C:/' or tea with polonium. Just choose fucking number...");
+                    System.out.println("I think, you need some 'format C:/' or tea with polonium. Just choose number...");
             }
+            System.out.println("Press any key to continue...");
+            String r = reader.readLine();
             for (int i = 0; i < 25; ++i) {
                 System.out.println();
-                Thread.sleep(100); // это для чего?
             }
         }
     }
