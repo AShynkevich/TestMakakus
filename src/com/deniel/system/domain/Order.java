9399835dacc6a3b4460bd9d97e79382e0ec1c75f
Deniel089 <deniel089@gmail.com>
2016-03-02 17:44:02 +0300

    Create domain package and Order class

---
+++ src/com/deniel/system/domain/Order.java
@@ -0,0 +1,43 @@
+package com.deniel.system.domain;
+
+/**
+ * Created by Deniel on 02.03.2016.
+ */
+public class Order {
+    int id;
+    String orderName;
+    int amount;
+    int price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
