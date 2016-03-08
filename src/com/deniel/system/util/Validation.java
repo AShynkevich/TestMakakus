5af92cd75b05fc6ab65be56ff454d7f8d6582ab4
Aliaksandr Shynkevich <al3xshaman@gmail.com>
2016-03-08 22:36:43 +0300

    alex-PR: Made validation util class

---
+++ src/com/deniel/system/util/Validation.java
@@ -0,0 +1,32 @@
+package com.deniel.system.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+/**
+ * Created by alexshaman on 3/7/16.
+ */
+public class Validation {
+    private Validation() {
+    }
+
+    public static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static String getValidString(String message, String regexp) {
+        Pattern pattern = Pattern.compile(regexp);
+        while (true) {
+            try {
+                System.out.println(message);
+                String line = reader.readLine();
+                if (pattern.matcher(line).matches()) {
+                    return line;
+                }
+                System.out.println("This key is not allowed here. Try again.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
