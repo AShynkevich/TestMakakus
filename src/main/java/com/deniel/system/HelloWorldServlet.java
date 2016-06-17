b986331a5de5ffcead35818c2e286ce60454ab23
Deniel089 <deniel089@gmail.com>
2016-06-17 12:06:09 +0300

    Small fix.

--- src/main/java/com/deniel/system/HelloWorldServlet.java
+++ src/main/java/com/deniel/system/HelloWorldServlet.java
@@ -13,19 +13,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HelloWorldServlet extends HttpServlet {
+    private static final String WEBINF_FMT = "/WEB-INF/jsp/{0}.jsp";
     private OrderService orderService = new OrderService();
-    private static final String WEBINF = "/WEB-INF/jsp/{0}.jsp";
 
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String adress = req.getRequestURI().substring(req.getContextPath().length());
 
         if (adress.equals("/")) {
-            getServletContext().getRequestDispatcher(MessageFormat.format(WEBINF, "hello")).forward(req, resp);
+            getServletContext().getRequestDispatcher(MessageFormat.format(WEBINF_FMT, "hello")).forward(req, resp);
         } else if (adress.equals("/getallorders")) {
             List<Order> orderList = orderService.getAll();
             req.setAttribute("orderList", orderList);
-            getServletContext().getRequestDispatcher(MessageFormat.format(WEBINF, "getallorders")).forward(req, resp);
+            getServletContext().getRequestDispatcher(MessageFormat.format(WEBINF_FMT, "getallorders")).forward(req, resp);
         } else {
             resp.sendRedirect("http://google.com");
         }
